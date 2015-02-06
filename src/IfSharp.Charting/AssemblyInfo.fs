9570a5b4c67cd624fd3a9a003350c76f1fd375da
David Carlson <mndrake22@mac.com>
2015-02-06 07:40:52 -0600

    v0.0.8

--- src/IfSharp.Charting/AssemblyInfo.fs
+++ src/IfSharp.Charting/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("IfSharp.Charting")>]
 [<assembly: AssemblyProductAttribute("IfSharp.Charting")>]
 [<assembly: AssemblyDescriptionAttribute("charting library for IfSharp using MetricsGraphics.js")>]
-[<assembly: AssemblyVersionAttribute("0.0.6")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.6")>]
+[<assembly: AssemblyVersionAttribute("0.0.8")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.8")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.6"
+    let [<Literal>] Version = "0.0.8"
