a806aa8cc50d32509fa6c377bb5455739781186d
David Carlson <mndrake22@mac.com>
2015-02-05 22:00:37 -0600

    histogram bug fix

--- src/IfSharp.Charting/AssemblyInfo.fs
+++ src/IfSharp.Charting/AssemblyInfo.fs
@@ -4,9 +4,9 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("IfSharp.Charting")>]
 [<assembly: AssemblyProductAttribute("IfSharp.Charting")>]
 [<assembly: AssemblyDescriptionAttribute("charting library for IfSharp using MetricsGraphics.js")>]
-[<assembly: AssemblyVersionAttribute("0.0.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.0.1")>]
+[<assembly: AssemblyVersionAttribute("0.0.4")>]
+[<assembly: AssemblyFileVersionAttribute("0.0.4")>]
 do ()
 
 module internal AssemblyVersionInformation =
-    let [<Literal>] Version = "0.0.1"
+    let [<Literal>] Version = "0.0.4"
